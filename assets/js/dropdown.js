607c6a36390f223a33ee6c15fa61bbc7f402219c
Markus Härnvi <markus@harnvi.net>
2026-05-06 18:53:54 +0200

    Anpassningar för SPT

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -17,61 +17,7 @@ function dropdown() {
     }
 
     const makeDropdown = function () {
-        if (mediaQuery.matches) return;
-        const submenuItems = [];
-
-        while ((nav.offsetWidth + 64) > menu.offsetWidth) {
-            if (nav.lastElementChild) {
-                submenuItems.unshift(nav.lastElementChild);
-                nav.lastElementChild.remove();
-            } else {
-                break;
-            }
-        }
-
-        if (!submenuItems.length) {
-            head.classList.add('is-dropdown-loaded');
-            return;
-        }
-
-        const toggle = document.createElement('button');
-        toggle.setAttribute('class', 'gh-more-toggle gh-icon-button');
-        toggle.setAttribute('aria-label', 'More');
-        toggle.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor"><path d="M21.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM13.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0zM5.333 16c0-1.473 1.194-2.667 2.667-2.667v0c1.473 0 2.667 1.194 2.667 2.667v0c0 1.473-1.194 2.667-2.667 2.667v0c-1.473 0-2.667-1.194-2.667-2.667v0z"></path></svg>';
-
-        const wrapper = document.createElement('div');
-        wrapper.setAttribute('class', 'gh-dropdown');
-
-        if (submenuItems.length >= 10) {
-            head.classList.add('is-dropdown-mega');
-            wrapper.style.gridTemplateRows = `repeat(${Math.ceil(submenuItems.length / 2)}, 1fr)`;
-        } else {
-            head.classList.remove('is-dropdown-mega');
-        }
-
-        submenuItems.forEach(function (child) {
-            wrapper.appendChild(child);
-        });
-
-        toggle.appendChild(wrapper);
-        nav.appendChild(toggle);
-
-        const toggleRect = toggle.getBoundingClientRect();
-        const documentCenter = window.innerWidth / 2;
-
-        if (toggleRect.left < documentCenter) {
-            wrapper.classList.add('is-left');
-        }
-
         head.classList.add('is-dropdown-loaded');
-
-        window.addEventListener('click', function (e) {
-            if (head.classList.contains('is-dropdown-open')) {
-                head.classList.remove('is-dropdown-open');
-            } else if (toggle.contains(e.target)) {
-                head.classList.add('is-dropdown-open');
-            }
-        });
     }
 
     imagesLoaded(logo, function () {
